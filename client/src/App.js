508f38abb45e59276f260a318082981362292b26
Tyler Maxwell <tylermaxwell1990@gmail.com>
2018-12-03 18:40:05 -0500

    redux auth actions implement started

--- client/src/App.js
+++ client/src/App.js
@@ -23,118 +23,28 @@ import "./App.css";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      // loggedIn: false,
-      // // Current user data
-      // user: {
-      //   id: "",
-      //   username: "",
-      //   firstName: "",
-      //   lastName: "",
-      //   email: ""
-      // }
-    };
-
-    // this.getUser = this.getUser.bind(this);
+    this.state = {};
     this.componentDidMount = this.componentDidMount.bind(this);
-    // this.updateUser = this.updateUser.bind(this);
-    // this.handleLogout = this.handleLogout.bind(this);
   }
 
   componentDidMount() {
-    this.props.getUser();
+    this.props.checkToken();
   }
-
-  // getUser() {
-  //   // Get JWT from local storage.
-  //   const token = localStorage.getItem("token");
-  //   console.log("getUser, token:", token);
-  //   // Pass token to secured route
-  //   UsersAPI.getCurrentUser(token).then(response => {
-  //     if (response.data.user) {
-  //       console.table(response.data.user);
-  //       this.setState({
-  //         loggedIn: true,
-  //         user: {
-  //           id: response.data.user._id,
-  //           username: response.data.user.username,
-  //           firstName: response.data.user.firstName,
-  //           lastName: response.data.user.lastName,
-  //           email: response.data.user.email
-  //         }
-  //       });
-  //     } else {
-  //       console.log("There is no user: ", response.data);
-  //       this.handleLogout();
-  //       this.setState({
-  //         user: null
-  //       });
-  //     }
-  //   });
-  // }
-
-  // updateUser(userObject) {
-  //   this.setState(userObject);
-  // }
-
-  // handleLogout(event) {
-  //   // event.preventDefault();
-  //   // Remove JWTs from local storage.
-  //   localStorage.removeItem("token");
-  //   localStorage.removeItem("refreshToken");
-  // UsersAPI.logoutUser({ user: this.state.username })
-  //   .then(response => {
-  //     if (response.status === 200) {
-  //       this.updateUser({
-  //         loggedIn: false,
-  //         user: null
-  //       });
-  //     }
-  //   })
-  //   .catch(error => {
-  //     console.log("Logout error", error);
-  //   });
-  // }
 
   render() {
     return (
       <React.Fragment>
         <Router>
           <Switch>
-            <Route
-              exact
-              path="/"
-              render={props => (
-                <Login updateUser={this.updateUser} {...props} />
-              )}
-            />
+            <Route exact path="/" render={props => <Login {...props} />} />
             <Route
               exact
               path="/signup"
-              render={props => (
-                <SignUp updateUser={this.updateUser} {...props} />
-              )}
+              render={props => <SignUp {...props} />}
             />
-            <AuthRoute
-              path="/dashboard"
-              component={PrivateMaster}
-              loggedIn={this.state.loggedIn}
-              handleLogout={this.handleLogout}
-              getUser={this.getUser}
-              user={this.state.user}
-            />
-            <AuthRoute
-              path="/account"
-              component={Account}
-              user={this.state.user}
-              handleLogout={this.handleLogout}
-            />
-            <AuthRoute
-              path="/password"
-              component={Password}
-              user={this.state.user}
-              handleLogout={this.handleLogout}
-            />
+            <AuthRoute path="/dashboard" component={PrivateMaster} />
+            <AuthRoute path="/account" component={Account} />
+            <AuthRoute path="/password" component={Password} />
             <Route render={() => <Redirect to="/dashboard" />} />
           </Switch>
         </Router>
@@ -152,7 +62,7 @@ class App extends React.Component {
 
 const mapDispatchToProps = dispatch => {
   return {
-    getUser: () => dispatch(actions.authToken())
+    checkToken: () => dispatch(actions.authToken())
   };
 };
 
