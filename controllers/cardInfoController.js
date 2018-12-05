b2aa3227d568fee860e863d8b70d35fe1659faba
Tyler Maxwell <tylermaxwell1990@gmail.com>
2018-12-05 16:42:58 -0500

    findByName complete

--- controllers/cardInfoController.js
+++ controllers/cardInfoController.js
@@ -1,1 +1,17 @@
 // Returns info from ../json
+const allSets = require("../json");
+
+module.exports = {
+  findByName: function(req, res) {
+    const foundCards = [];
+    allSets.forEach(set => {
+      set.cards.forEach(card => {
+        if (card.name.toLowerCase().includes(req.params.name)) {
+          foundCards.push(card);
+        }
+      });
+    });
+    res.json(foundCards);
+  },
+  findById: function(req, res) {}
+};
